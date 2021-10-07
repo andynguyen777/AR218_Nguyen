b6dcad8d5fd816416e7e26372c7ee04c2af73baf
andynguyen777 <90798065+andynguyen777@users.noreply.github.com>
2021-10-07 16:47:15 -0400

    Update

--- Platformer Godot/scene/Player.gd
+++ Platformer Godot/scene/Player.gd
@@ -1,38 +1,33 @@
 extends KinematicBody2D
 
-const GRAVITY = 35
-
-var velocity= Vector2(0,0) 
-
-export var speed = 100
-export var jump = -900
-
+var velocity := Vector2(0,0)
+export var speed := 180
+export var jump := -900
+const GRAVITY := 35
 
 func _physics_process(delta):
-	if Input.is_action_pressed("left"):
-		velocity.x=-speed
-		$AnimatedSprite.play("walk") 
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+		$AnimatedSprite.play("walk")
 		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("right"): 
-		velocity.x=speed
-		$AnimatedSprite.play("walk") 
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+		$AnimatedSprite.play("walk")
 		$AnimatedSprite.flip_h = true
-	else: 
-		velocity.x=0
-	
-	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y += jump 
-	
-	if not is_on_floor(): 
+	else:
+		$AnimatedSprite.play("idle")
+	#show with "pressed"
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += jump
+		
+	if not is_on_floor():
 		$AnimatedSprite.play("air")
-		#get_node(AnimatedSprite) 
-		velocity.y += GRAVITY 
-	
-	
-	velocity.x = lerp(velocity.x, 0, 0.1)
-	move_and_slide(velocity)
-	
+
+	velocity.y += GRAVITY
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	velocity.x = lerp(velocity.x, 0,0.1)
 
 
 func _on_Area2D_area_entered(area):
